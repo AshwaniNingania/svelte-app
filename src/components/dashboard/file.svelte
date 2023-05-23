5dc9dbe76d4be64c5f810642603db338e7ec2943
ashwani01 <ashwani01@nagarro.com>
2023-05-23 19:14:14 +0530

    final commit

--- src/components/dashboard/file.svelte
+++ src/components/dashboard/file.svelte
@@ -2,12 +2,13 @@
     export let name;
 </script>
 
-<span style="background-image: url(/src/assets/file.svg)">{name}</span>
+<span>{name}</span>
 
 <style>
     span {
         padding: 0 0 0 1.5em;
         background: 0 0.1em no-repeat;
         background-size: 1em 1em;
+        background-image: url(/src/assets/file-icon.svg);
     }
 </style>
